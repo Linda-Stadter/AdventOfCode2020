a4b30b6a14415cc8f46bcf4e9234582dbad9f207
Linda <linda.stadter@fau.de>
2020-12-06 01:52:41 +0100

    Day 3

---
+++ Day 3/solution.py
@@ -0,0 +1,34 @@
+input_path = "input.txt"
+
+
+def check_trees(slope_x, slope_y):
+
+    trees = 0
+    pos_x = 0
+
+    with open(input_path, "r") as input_file:
+        input_lines = input_file.readlines()
+        input_lines = [x.strip() for x in input_lines]
+
+    for line in input_lines[::slope_y]:
+        line = list(line)
+        if line[pos_x % len(line)] == "#":
+            trees += 1
+        pos_x += slope_x
+
+    return trees
+
+
+def part_two():
+
+    print(check_trees(1, 1) * check_trees(3, 1) *
+          check_trees(5, 1) * check_trees(7, 1) * check_trees(1, 2))
+
+
+def part_one():
+
+    print(check_trees(3, 1))
+
+
+part_one()
+part_two()
