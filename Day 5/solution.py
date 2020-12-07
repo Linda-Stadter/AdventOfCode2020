ae44b12216504e54c04178e2cb3e9ba2ac6f9095
Linda <linda.stadter@fau.de>
2020-12-07 15:59:36 +0100

    Day 5

---
+++ Day 5/solution.py
@@ -0,0 +1,46 @@
+import re
+input_path = "input.txt"
+
+
+def binary_space_search(line, lower_key):
+    min_row = 0
+    max_row = 2**len(line) - 1
+    for c in line[:]:
+        mid = (min_row + max_row) // 2
+        if c == lower_key:
+            max_row = mid
+        else:
+            min_row = mid + 1
+    return min_row
+
+
+def part_one():
+    all_ids = []
+
+    with open(input_path, "r") as input_file:
+        input_lines = input_file.readlines()
+        input_lines.append("\n")
+        input_lines = [x.strip() for x in input_lines]
+
+    for line in input_lines:
+        row = binary_space_search(line[:7], "F")
+        col = binary_space_search(line[7:], "L")
+        all_ids.append(row * 8 + col)
+
+    return all_ids
+
+
+def part_two():
+    ids = part_one()
+    possible = range(127*8+7)
+    missing = list(set(possible) - set(ids))
+    missing = sorted(missing)
+
+    for i in range(1, len(missing)-1):
+        if missing[i-1]+1 != missing[i] and missing[i]+1 != missing[i+1]:
+            print(missing[i])
+
+
+ids = part_one()
+print(max(ids))
+part_two()
